3967c0413862beaa564dbd8da3cccae4bd4fa7d4
ReneZero <85256822+ReneZero@users.noreply.github.com>
2021-06-02 13:27:08 -0700

    Add files via upload

---
+++ offboardCSV.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# User Offboarding Script
+
+GAM=~/bin/gamadv-xtd3/gam
+
+start_logger() {
+    exec &> >(tee offboard.log)
+    echo "$(whoami) conducting offboarding on $(date)"
+}
+
+suspend_user() {
+    ${GAM} csv $csvFile gam update user "~email" suspended on
+        echo "Suspending user and moving to z-Archive OU"
+        ${GAM} csv $csvFile gam update org 'z-Archive' add users "~email"
+        echo "Suspending"
+}
+
+disable_user() {
+    echo "Disabling Email and hiding from Directory"
+    ${GAM} csv $csvFile gam user "~email" forward off
+    ${GAM} csv $csvFile gam user "~email" imap off
+    ${GAM} csv $csvFile gam user "~email" pop off
+    ${GAM} csv $csvFile gam update user "~email" gal off
+}
+remove_license() {
+    echo "Removing Education Plus License & OR Google Voice License"
+    ${GAM} csv $csvFile gam user "~email" delete license 1010310002
+    ${GAM} csv $csvFile gam user "~email" delete license 1010330004
+}
+
+remove_groups() {
+    echo "Removing user from all groups"
+    ${GAM} csv $csvFile gam info user "~email" | grep -A 10000 "Groups:" | grep -i -o '[A-Z0-9._%+-]\+@[A-Z0-9.-]\+\.[A-Z]\{2,4\}' >/tmp/remove.txt
+    while read -r GROUP; do 
+        [ -z "$GROUP" ] && continue
+        ${GAM} csv $csvFile gam update group "${GROUP}" remove member "~email"
+    done </tmp/remove.txt
+}
+
+echo "Please drag csv file here: "
+
+read csvFile
+
+start_logger
+remove_groups
+disable_user
+remove_license
+suspend_user
---
+++ onboardSingleUser.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+# User Onloading
+
+GAM=~/bin/gamadv-xtd3/gam
+
+echo "PLease enter the staff's first name: "
+
+read firstName
+
+echo "Please enter staff's Lastname: "
+
+read lastName
+
+echo "Please enter the email you wish to give them: "
+
+read staffEmail
+
+start_logger() {
+    exec &> >(tee onboard.log)
+    echo "$(whoami) conducting offboarding for $staffEmail on $(date)"
+}
+
+email_verification() {
+    echo "Checking to see if user exists"
+    if ${GAM} info user "${staffEmail}" >/dev/null 2>&1; then
+        printf %s\\n "Please try again and enter a valid email address. This one is already taken"
+        exit 1
+    else
+        echo "$staffEmail does not exist in Google Suite. We are clear to proceed"
+    fi
+    printf %s\\n "Going to next step"
+
+}
+
+create_email() {
+    echo "creating email for $staffEmail"
+    ${GAM} create user "${staffEmail}"
+    ${GAM} update user "${staffEmail}" givenname "${firstName}"
+    ${GAM} update user "${staffEmail}" familyname "${lastName}"
+    echo "succefully created user"
+    
+}
+
+
+reset_password() {
+    echo "Resetting GSuite password to R3setPWD!"
+    PASSWORD=R3setPWD!
+    ${GAM} update user "${staffEmail}" password "${PASSWORD}"
+    ${GAM} update user "${staffEmail}" changepassword on
+}
+
+change_ou() {
+    echo "Which site is staff working at? Please enter a number"
+    echo -n "[1]BCCS [2]Morcs [3]BCCHS [4]LSC: "
+    read VAR
+if [[ $VAR -eq 1 ]]
+then
+  echo "changing to BCCS Staff OU"
+  ${GAM} update org '/Staff/BCCS Staff' add users "${staffEmail}"
+elif [[ $VAR -eq 2 ]]
+then
+  echo "changing to MORCS Staff OU"
+  ${GAM} update org '/Staff/MORCS Staff' add users "${staffEmail}"
+elif [[ $VAR -eq 3 ]]
+then
+  echo "changing to BCCHS Staff OU"
+  ${GAM} update org '/Staff/BCCHS Staff' add users "${staffEmail}"
+elif [[ $VAR -eq 4 ]]
+then
+  echo "changing to LSC Staff OU"
+  ${GAM} update org '/Staff/LSC' add users "${staffEmail}"
+else
+  echo "did not enter a correct number"
+  exit 1
+fi
+}
+
+add_license() {
+    echo "does this account need a enterprise license"
+    echo -n "[1]Yes [2]No"
+    read VAR
+if [[ $VAR -eq 1 ]]
+then
+  echo "Applying Education Plus License"
+  ${GAM} user "${staffEmail}" add license 1010310002
+elif [[ $VAR -eq 2 ]]
+then
+  echo "license skipped"
+else
+  echo "did not enter a correct number"
+  exit 1
+fi
+}
+add_Voicelicense() {
+    echo "does this account need a Voice license"
+    echo -n "[1]Yes [2]No"
+    read VAR
+if [[ $VAR -eq 1 ]]
+then
+  echo "Applying Voice license"
+  ${GAM} user "${staffEmail}" add license 1010330004
+elif [[ $VAR -eq 2 ]]
+then
+  echo "license skipped"
+else
+  echo "did not enter a correct number"
+  exit 1
+fi
+}
+
+email_verification
+start_logger
+create_email
+reset_password
+change_ou
+add_license
+add_Voicelicense
+
