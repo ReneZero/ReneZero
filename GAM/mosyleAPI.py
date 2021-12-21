b0dfff0001e50a5dfa1d51d947c270038b8bb1f3
Rene Sanchez <renesanchez3005@gmail.com>
2021-12-20 22:33:08 -0800

    add mosyle and SnipeIT Account Creation

---
+++ GAM/mosyleAPI.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import sys
+
+# get arguments from onboard script and pass them to variables
+newID = str(sys.argv[1])
+newUserEmail = str(sys.argv[1])
+schoolSite = str(sys.argv[2])
+teacherOrStaff = str(sys.argv[3])
+newUserName = str(sys.argv[4]) + " " + str(sys.argv[5])
+
+#JSON template from mosyle to create a user
+payload = '''
+{
+    "accessToken": "c8a5d56d90a0a21d10218906cd4424a52dcc54a154c1c826958255188d5421ad",
+    "elements": [
+    {
+        "id": "aaa",
+        "operation": "save",
+        "name": "User Staff Test",
+        "type": "STAFF",
+        "welcome_email": 0,
+        "email": "ReneAPItestv2@renizzle818.com",
+
+        "locations": [
+            {
+                "name": "BCCS"
+            }
+        ]
+    }
+    ]
+}
+'''
+# Converts the raw JSON to a python object so we can work with it
+dataToSend = json.loads(payload)
+
+#for loop to cycle through the newly created python object so we can find and replace
+#the exact values
+#replaces the template data with the passed in arguements
+for person in dataToSend['elements']:
+        person['id'] = newID
+        person['locations'] = [{'name': schoolSite}]
+        person['type'] = teacherOrStaff
+        person['email'] = newUserEmail
+        person['name'] = newUserName
+        
+        
+#puts the object into a big string
+finishedData = json.dumps(dataToSend)
+
+# sends a post request to mosyle API with the modified template finishedData string as the payload        
+r = requests.post('https://managerapi.mosyle.com/v2/users', data=finishedData)
+#prints the JSON that comes back from the API
+#If "status":"OK" then user was created successfully
+exitCode = int(r.status_code)
+
+if exitCode == 200:
+    print("User was sucesfully created")
+else:
+    print("User was not created sucesfully")
