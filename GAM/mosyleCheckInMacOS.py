06afcb5a05fbf46a7201902cd5bb626ae8809200
Rene Sanchez <renesanchez3005@gmail.com>
2021-12-22 00:18:09 -0800

    added checkin for IOS working on MacOS

---
+++ GAM/mosyleCheckInMacOS.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import sys
+
+# get arguments from onboard script and pass them to variables
+userID = str(sys.argv[1])
+uniqueID = "0"
+counter = 2
+
+def checkInMacOS():
+    #JSON template from mosyle to create a user
+    payload ='''
+    {
+        "accessToken": "c8a5d56d90a0a21d10218906cd4424a52dcc54a154c1c826958255188d5421ad",
+        "options": {
+            "os": "mac",
+            "page": "1",
+            "specific_columns": [
+                "userid",
+                "serial_number",
+                "deviceudid"
+            ]
+        }
+    }
+    '''
+    payload2 = '''
+    {
+    "accessToken": "c8a5d56d90a0a21d10218906cd4424a52dcc54a154c1c826958255188d5421ad",
+    "elements": [
+        {
+            "operation": "change_to_limbo",
+            "devices": [
+                "00008027-000C45693A6A402E"
+                ]
+            }
+        ]
+    }
+    '''
+    editPayload = json.loads(payload)
+        
+    #### Getting list of devices
+    r = requests.post('https://managerapi.mosyle.com/v2/listdevices', data=payload)
+    json_data = json.loads(r.text)
+    ####Filtering until we find the one tied to their email
+    for device in json_data['response']['devices']:
+        if 'userid' in device:
+            if device['userid'] == userID:
+                ####grabbing deviceUDID
+                uniqueID = device['deviceudid']
+                print(uniqueID)
+            else:
+                while device['userid'] != userID:
+                    for test in editPayload['options']['page']:
+                        test=str(counter)
+                pageUp = json.dumps(editPayload)
+                r3 = requests.post('https://managerapi.mosyle.com/v2/listdevices', data=pageUp)
+                r3Response = json.loads(r3.text)
+                for device in r3Response['response']['devices']:
+                    if 'userid' in device:
+                        if device['userid'] == userID:
+                        ####grabbing deviceUDID
+                            uniqueID = device['deviceudid']
+                            print(uniqueID)
+                
+                    
+    
+    # Converts the raw JSON to a python object so we can work with it
+    dataToSend = json.loads(payload2)
+    #########Replacing UDID with the one we grabbed
+    # for device2 in dataToSend['elements']:
+    #     device2['devices'] = [uniqueID]        
+    #puts the object into a big string
+    finishedData = json.dumps(dataToSend)
+    ##########sending API Request in order to change the device to limbo using the UDID
+    # r2 = requests.post('https://managerapi.mosyle.com/v2/bulkops', data=finishedData)
+    # print(r2.text)
+    #######################################################################
+    
+checkInMacOS()
